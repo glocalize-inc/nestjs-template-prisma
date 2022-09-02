88ebf9af6e7a51a57fb2cb94559f9f6f08da2a12
Terry <yungik@glocalizeinc.com>
2022-09-02 12:34:10 +0900

    Add common module params to scheduler

--- src/root/scheduler/scheduler.module.ts
+++ src/root/scheduler/scheduler.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common'
 import { ScheduleModule } from '@nestjs/schedule'
-import { CommonModule } from 'src/root/common/common.module'
+import { CommonModule, CommonModuleParams } from 'src/root/common/common.module'
 import { ExampleSchedulerModule } from './example-scheduler/example-scheduler.module'
 
+export type SchedulerModuleParams = { commonModuleParams?: CommonModuleParams }
 @Module({})
 export class SchedulerModule {
-  static forRoot() {
+  static forRoot({ commonModuleParams }: SchedulerModuleParams = {}) {
     return {
       module: SchedulerModule,
       imports: [
-        CommonModule.forRoot(),
+        CommonModule.forRoot(commonModuleParams),
         ScheduleModule.forRoot(),
         ExampleSchedulerModule,
       ],
